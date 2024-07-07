e65aa8a14c048d0d4a28d49a905dad7d5cbac332
Yeeun_An <48678872+yeeSilver@users.noreply.github.com>
2024-07-07 19:52:54 +0900

    Title: 갈 수 있는 곳들, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240707/갈 수 있는 곳들/places-can-go.py
+++ 240707/갈 수 있는 곳들/places-can-go.py
@@ -9,36 +9,41 @@ grid=[
 visited=[
     [False for _ in range(n)] for _ in range(n)
 ]
+
 def in_range(r,c):
     return 0<=r<n and 0<=c<n
 def can_go(r,c):
-    if not in_range: return False
+    if not in_range(r,c): return False
     if visited[r][c]: return False
     if grid[r][c] == 1: return False
     return True
 
-def bfs():
+def bfs(x,y):
     global area
-    while q:
-        x,y = q.popleft()
-        dxs,dys= [0,0,-1,1],[1,-1,0,0]
-        for dx,dy in zip(dxs,dys):
-            nx = x+dx 
-            ny = y+dy
-            if can_go(nx,ny):
-                visited[nx][ny]=True
-                area+=1
-                q.append((nx,ny))
 
+    dxs,dys= [0,0,-1,1],[1,-1,0,0]
+    for i in range(4):
+        nx = x+dxs[i]
+        ny = y+dys[i]
+        if can_go(nx,ny):
+            visited[nx][ny]=True
+            area+=1
+
+def initalized():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
 
 area = 0
 for _ in range(k):
     r,c = map(int,input().split())
     r-=1
     c-=1
+
+    initalized()
+
     if can_go(r,c):
         visited[r][c]=True
         area+=1
-        q.append((r,c))
-        bfs()
+        bfs(r,c)
 print(area)
