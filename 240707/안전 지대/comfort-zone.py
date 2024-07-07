725daccc0ce3c1265cabd5990152de5592d9201d
Yeeun_An <48678872+yeeSilver@users.noreply.github.com>
2024-07-07 15:48:33 +0900

    Title: 안전 지대, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240707/안전 지대/comfort-zone.py
+++ 240707/안전 지대/comfort-zone.py
@@ -1,7 +1,7 @@
 import sys
 sys.setrecursionlimit(10000)
 
-n,m = map(int,input().split())
+n,m = tuple(map(int,input().split()))
 grid= [
     list(map(int,input().split())) for _ in range(n)
 ]
@@ -16,10 +16,10 @@ def initalized_visited():
         for j in range(m):
             visited[i][j] = False
 
-# def in_range(i, j):
-#     return 0 <= i < n and 0 <= j < m
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < m
+def in_range(i, j):
+    return 0 <= i < n and 0 <= j < m
+# def in_range(x, y):
+#     return 0 <= x and x < n and 0 <= y and y < m
 
 def can_go(i, j, k):
     if not in_range(i, j): return False
